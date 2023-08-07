4a98345015e2793c6604f7129e7d3dc3fc2a8a67
jenishkarkar77 <jenishkarkar77@gmail.com>
2023-08-07 14:48:29 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let [s1,sets1] = useState()
+  let [s2,sets2] = useState()
+  let [main,setmain] = useState([])
+
+
+  const btnhandler = () =>{
+
+    
+    const obj = {
+      s1:s1,
+      s2:s2
+    }
+      setmain([...main,obj])
+
+
+      console.log(main);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' onChange={(e)=>{ sets1(e.target.value) }}></input>
+      <input type='text' onChange={(e)=>{ sets2(e.target.value) }}></input>
+      <input type='button' value="Click Here" onClick={btnhandler}></input>
+
+
+      {
+          main.map((x)=>{
+            return(
+              <>
+                <h1>S1 : {x.s1}</h1>
+              </>
+            )
+          })
+      }
+
     </div>
   );
 }
